555ad6e3d3b7d10fde178319c3f1ce7cae3be0b4
Audimmk <audymakore@gmail.com>
2026-06-10 05:28:52 +0200

    Fix AuthContext import paths

--- mobile/streym/src/screens/passenger/ProfileScreen.js
+++ mobile/streym/src/screens/passenger/ProfileScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, } from 'react-native';
-import { useAuth } from '../../contexts/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 import { COLORS } from '../../utils/constants';
 
 const ProfileScreen = () => {
